3db69a46cc700f8a602a1270acf41af647ac7bc7
unknown <Artem@Artem-Laptop.(none)>
2012-01-23 10:20:56 -0500

    Removed project files

--- HelloWorld/main.d
+++
@@ -1,8 +0,0 @@
-main.o: main.c
-main.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
-main.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-main.o: C:\Keil\ARM\RV31\Inc\stdint.h
-main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-main.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
-main.o: uart_polling.h
--- HelloWorld/system_lpc17xx.d
+++
@@ -1,7 +0,0 @@
-system_lpc17xx.o: system_LPC17xx.c
-system_lpc17xx.o: C:\Keil\ARM\RV31\Inc\stdint.h
-system_lpc17xx.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
-system_lpc17xx.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-system_lpc17xx.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-system_lpc17xx.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-system_lpc17xx.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h

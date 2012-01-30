1cbfb25b09d7d4da98de48be6a4c812a4bdc68dc
Robert Elder <robert@robertelder.ca>
2012-01-29 23:17:07 -0500

    Added robust memory testing, also, added random number function, pow function, and print number functions

--- Lab1/src/main.c
+++ Lab1/src/main.c
@@ -13,12 +13,84 @@ unsigned int free_mem = (unsigned int) &Image$$RW_IRAM1$$ZI$$Limit;
 
 int * pMaxNumberOfMemoryBlocksEverAllocated;
 
+int RANDOM_NUMBER_GENERATOR_m_w = 7;    /* must not be zero */
+int RANDOM_NUMBER_GENERATOR_m_z = 123;    /* must not be zero */
+
+
 void assert(int value, unsigned char * message){
 	if(value == 0){
-		uart0_put_string("\nTHERE WAS AN ASSERTION FAILURE!!!\n\n");
+		uart0_put_string("\nTHERE WAS AN ASSERTION FAILURE!!!\n");
 		uart0_put_string(message);
+		uart0_put_string("\nAbnormal program termination.\n");
+		// I can't figure out any other way to get it to stop executing.
+		while (1) {}
 	}
 }
+ 
+unsigned int get_random(){
+	/* This is a shitty way to generate random numbers but it works for what we need */
+    RANDOM_NUMBER_GENERATOR_m_z = 36969 * (RANDOM_NUMBER_GENERATOR_m_z & 65535) + (RANDOM_NUMBER_GENERATOR_m_z >> 16);
+    RANDOM_NUMBER_GENERATOR_m_w = 18000 * (RANDOM_NUMBER_GENERATOR_m_w & 65535) + (RANDOM_NUMBER_GENERATOR_m_w >> 16);
+    return (RANDOM_NUMBER_GENERATOR_m_z << 16) + RANDOM_NUMBER_GENERATOR_m_w;  /* 32-bit result */
+}
+
+unsigned int pow(unsigned int base, unsigned int exponent){
+	// Big surprise we have to write our own pow function
+	if(exponent == 0)
+		return 1;
+	else
+		return base * pow(base,exponent - 1);
+}
+
+void printDigit(unsigned int i){
+	unsigned char digits[] = "0123456789";
+	assert(i < 10, "This should not happen");
+	//Put a null after the digit we want so that we only print that digit
+	digits[i+1] = 0;
+	uart0_put_string(&(digits[i]));
+}
+
+void print_unsigned_integer(unsigned int i){
+
+	int hasSeenADigitYet = 0;
+	// Max int is 4,294,967,295
+	int currentExponent = 9;
+
+	if(i == 0){
+		printDigit(i);
+		return;
+	}
+
+	while(currentExponent >= 0){
+		// What value does a digit have for this exponent? 
+		int currentDigitValue = pow(10,currentExponent); 
+		//  What is the value for this digit?
+		int currentDigit = i / currentDigitValue;
+		//  We have accounted for this quantity in our representation, remove it
+		i -= currentDigit * currentDigitValue;
+
+		//  Don't print leading 0's but do print interior zeros
+		if(hasSeenADigitYet || currentDigit){
+			//  Always print 0's now
+			hasSeenADigitYet = 1;
+			//  Determines if P = NP
+			printDigit(currentDigit);
+		}
+		// Next value 
+		currentExponent--;
+	}
+}
+
+void print_signed_integer(signed int i){
+	if(i < 0){
+		uart0_put_string("-");
+		print_unsigned_integer((unsigned int)(-i));
+	}else{
+		print_unsigned_integer((unsigned int)i);		
+	}
+}
+
+
 
 void * get_address_of_memory_block_at_index(int memoryBlockIndex){
 	//  taken up by pMaxNumberOfMemoryBlocksEverAllocated plus the offset of the byte for that memory block
@@ -65,9 +137,12 @@ void * request_memory_block (){
 
 int release_memory_block (void * MemoryBlock){
 	// Whatever value we are given it should be inside the range of possible allocated blocks
-	int memoryBlockOffset = ((int)MemoryBlock) - START_OF_ALLOCATABLE_MEMORY;
+	//  I don't know why you can't just subtract the constant, but for some reason, when you do it will add 0x4000 to the result: WTF?
+	int startOfAllocatableMemory = START_OF_ALLOCATABLE_MEMORY;
+	int memoryBlockOffset = ((unsigned int)MemoryBlock) - startOfAllocatableMemory;
 	int memoryBlockIndex = 0;
 	char * pAllocationStatusByte = (char *)0;
+
 	// Make sure it is a valid pointer
 	if(memoryBlockOffset % MEMORY_BLOCK_SIZE != 0){
 		assert(0,"Invalid memory address to release, not divisible by memory block size\n");
@@ -94,21 +169,99 @@ void init_memory_allocation_table(){
 	*pMaxNumberOfMemoryBlocksEverAllocated = 0;
 }
 
+
+
 void run_memory_tests(){
-	// This test assumes that they were allocated in consecutive order
 
-	int i;
+	int testCases = 100000;
+	int currentTestCase = 0;
+	int tmpCounter = 0;
+	int testValue1 = 6;
+	int testValue2 = 57;
+	int * pTestPointer1 = 0;
+	int * pTestPointer2 = 0;
 
+	int numberOfPointersYouCanPutInOneBlockOfMemory = MEMORY_BLOCK_SIZE / sizeof(int);
 
-	for(i=0; i < 10; i++){
-		int * pInt = request_memory_block();
-		assert((int)pInt, "Request memory returned null");
-		*pInt = i;
+	//Allocate one block of memory
+	pTestPointer1 = request_memory_block();
+	assert((int)pTestPointer1, "Request memory returned null");
+	*pTestPointer1 = testValue1;
+	assert((*pTestPointer1) == testValue1, "the first memory test failed.");
+
+	//Allocate a second block of memory
+	pTestPointer2 = request_memory_block();
+	assert((int)pTestPointer2, "Request memory returned null");
+	*pTestPointer2 = testValue2;
+	assert((*pTestPointer2) == testValue2, "the second memory test failed.");
+
+	// Make sure this memory block is clean, even thought I think it already is
+	for(tmpCounter = 0; tmpCounter < numberOfPointersYouCanPutInOneBlockOfMemory; tmpCounter++){
+		pTestPointer1[tmpCounter] = 0;
 	}
 
-	for(i=0; i < 10; i++){
-		int * pInt = get_address_of_memory_block_at_index(i);
-		assert((*pInt) == i, "the first memory test failed.");
+	for(currentTestCase = 0; currentTestCase < testCases; currentTestCase++){
+		// Randomly pick an int that we will consider to be a pointer to another memory block
+		int currentMemoryBlockIndex = get_random() % numberOfPointersYouCanPutInOneBlockOfMemory;
+		//  Treat the int at that location as a pointer to memory somewhere
+		int ** pointerToCurrentMemoryBlockPointer = (int**)&(pTestPointer1[currentMemoryBlockIndex]);
+		//  If that address points to nothing, allocate new memory and put the pointer there
+		if(*pointerToCurrentMemoryBlockPointer == (int *)0){
+			*pointerToCurrentMemoryBlockPointer = (int *)request_memory_block();
+			//uart0_put_string("Test case ");print_unsigned_integer(currentTestCase);	uart0_put_string(" of "); print_unsigned_integer(testCases);uart0_put_string(": ");
+			//uart0_put_string("Allocated memory block: ");print_unsigned_integer((int)(*pointerToCurrentMemoryBlockPointer));uart0_put_string("\n\r");
+			for(tmpCounter = 0; tmpCounter < numberOfPointersYouCanPutInOneBlockOfMemory; tmpCounter++){
+				// Fill this new block of memory with the ints that are the address of the block itself plus the offset(tmpCounter) of the that int 
+				(*pointerToCurrentMemoryBlockPointer)[tmpCounter] = ((int)(*pointerToCurrentMemoryBlockPointer)) + tmpCounter;
+			}		
+		}else{
+			//  This block does point to something, verify that the data there is in the expected format, then delete it
+			for(tmpCounter = 0; tmpCounter < numberOfPointersYouCanPutInOneBlockOfMemory; tmpCounter++){
+				// Is what we put there still the same?
+				assert((*pointerToCurrentMemoryBlockPointer)[tmpCounter] == ((int)(*pointerToCurrentMemoryBlockPointer)) + tmpCounter,"Memory test failure: block failed sanity check.");
+			}
+			//  Delete this block
+			//uart0_put_string("Test case ");print_unsigned_integer(currentTestCase);	uart0_put_string(" of "); print_unsigned_integer(testCases);uart0_put_string(": ");
+			//uart0_put_string("Memory block ");print_unsigned_integer((int)(*pointerToCurrentMemoryBlockPointer));uart0_put_string(" looks OK, releasing.\n\r");
+			release_memory_block(*pointerToCurrentMemoryBlockPointer);
+			*pointerToCurrentMemoryBlockPointer = 0;
+		}
+	}
+}
+
+void print_some_numbers(){
+	int i;
+
+	print_unsigned_integer(5002);
+	uart0_put_string("\n\r");
+	print_unsigned_integer(542342);
+	uart0_put_string("\n\r");
+	print_unsigned_integer(782342);
+	uart0_put_string("\n\r");
+	print_unsigned_integer(5543242);
+	uart0_put_string("\n\r");
+	print_unsigned_integer(544342);
+	uart0_put_string("\n\r");
+	print_unsigned_integer(5442342);
+	uart0_put_string("\n\r");
+	print_signed_integer(-42);
+	uart0_put_string("\n\r");
+	print_signed_integer(0);
+	uart0_put_string("\n\r");
+	print_signed_integer(-0);
+	uart0_put_string("\n\r");
+	print_signed_integer(2);
+	uart0_put_string("\n\r");
+	print_signed_integer(666);
+	uart0_put_string("\n\r");
+	print_signed_integer(345303);
+	uart0_put_string("\n\r");
+	print_signed_integer(-402);
+	uart0_put_string("\n\r");
+
+	for(i = 0; i < 10; i++){
+		print_unsigned_integer(get_random() % 200);
+		uart0_put_string("\n\r");
 	}
 }
 
@@ -118,6 +271,7 @@ int	main(){
 	uart0_put_string("Hello World!\n\r");
 	init_memory_allocation_table();
 	run_memory_tests();
+	print_some_numbers();
 	uart0_put_string("\nProgram Terminated normally\n\r");
 	return 0;
 }

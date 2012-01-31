0717f30759d6e988588eb27fe261a00f941d9269
Robert Elder <robert@robertelder.ca>
2012-01-30 21:39:52 -0500

    Hope this works

--- Lab1/startup_LPC17xx.d
+++
@@ -1,1 +0,0 @@
-startup_LPC17xx.o: src\startup_LPC17xx.s
--- Lab1/uart_polling.d
+++
@@ -1,8 +0,0 @@
-uart_polling.o: src\uart_polling.c
-uart_polling.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
-uart_polling.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-uart_polling.o: C:\Keil\ARM\RV31\Inc\stdint.h
-uart_polling.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-uart_polling.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-uart_polling.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
-uart_polling.o: src\uart_polling.h

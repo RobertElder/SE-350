0296c48f674896468869d14943b6bd6b91cb34b3
Artem Pasyechnyk <artem.i.p@gmail.com>
2012-03-05 21:48:48 -0500

    Timing services implementation

--- Lab1/src/iprocess.c
+++ Lab1/src/iprocess.c
@@ -2,7 +2,7 @@
 #include "timer.h"
 #include "ipc.h"
 
-QueueHead delayed_messages;
+LinkedList delayed_messages;
 
 typedef struct delayedMessage {
 	int pid;
@@ -16,32 +16,69 @@ uint32_t get_current_time() {
 	return g_timer_count;
 }
 
+void expiry_sorted_enqueue(LinkedList* listHead, ListNode* node) {
+	ListNode* currentNode = listHead->head;
+
+	if(currentNode == NULL) {
+	 	(* listHead).head = node;
+		(* listHead).tail = node;
+		return;
+	}
+
+	//if node.expiry < head.expiry (insert at beginning)														   
+	if(((DelayedMessage *)currentNode->data)->expiry_time > ((DelayedMessage *)node->data)->expiry_time) {
+		(* node).next = listHead->head;
+		(* listHead).head = node;
+	}
+
+	//while curr.expiry > node.expiry
+	while(((DelayedMessage *)currentNode->data)->expiry_time < ((DelayedMessage *)node->data)->expiry_time) {
+		currentNode = currentNode->next;
+	}
+
+	//insert in proper location
+	(* node).next = currentNode->next;
+	(* currentNode).next = node;
+}
+
 int delayed_send(int pid, Envelope * envelope, int delay) {
 	DelayedMessage * m;
+	ListNode * node;
+
 	m->pid = pid;
 	m->envelope = envelope;
 	m->expiry_time = get_current_time() + delay;
+	
+	node->data = &m;
+	node->next = NULL;
 
-	//add m to timeoutProcess message queue
+	//add node containing m to timeoutProcess message queue
+	expiry_sorted_enqueue(&delayed_messages, node);
 
 	return 0;
 }
 
 void timeout_i_process() {
-	Envelope * env;// = receive_message();
+	Envelope * env = receive_message(NULL);
+	ListNode * node;
+	int receiver_pid;
 
 	while(env != NULL) {
-	 	//enqueue(&delayed_messages, env);
-		//env = receive_message();
+		node->data = &env;
+		node->next = NULL;
+
+	 	expiry_sorted_enqueue(&delayed_messages, node);
+
+		env = receive_message(NULL);
 	}
 
 	if(delayed_messages.head != NULL) {
-//		while (head_of_timeout_list.expiry_time <= cur_time)
-//		{
-//			env = timeout_list.dequeue();
-//			target_pid = env.destination_pid;
-//			send( target_pid, env ); //forward msg to destination
-//		}						
+		while (((DelayedMessage *)(delayed_messages.head->data))->expiry_time <= get_current_time())
+		{
+			env = ((DelayedMessage *)dequeue(&delayed_messages)->data)->envelope;
+			receiver_pid = env->receiver_pid;
+			send_message( receiver_pid, env ); //forward msg to destination
+		}						
 	}
 
 }

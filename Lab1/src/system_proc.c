c198eda60bcab1dc61c16e142f19dfa873dcaea4
Robert Elder <robert@robertelder.ca>
2012-03-09 09:56:39 -0500

    Added some API functions for registering a command

--- Lab1/src/system_proc.c
+++ Lab1/src/system_proc.c
@@ -6,14 +6,92 @@
 #include "process.h"
 #include "ipc.h"
 
-// These are dummy variables so this code will compile.  This code is nowhere near finished
+#define	MAX_NUMBER_OF_REGISTERABLE_COMMANDS             10
+#define	MAX_COMMAND_LENGTH                              500
 
-extern uint8_t g_UART0_TX_empty_placeholder=1;
+int number_of_registered_commands = 0;
+char registered_commands[MAX_NUMBER_OF_REGISTERABLE_COMMANDS][MAX_COMMAND_LENGTH];
+int registered_processes[MAX_NUMBER_OF_REGISTERABLE_COMMANDS];
+
+char current_command_buffer[MAX_COMMAND_LENGTH];
+int current_command_length = 0;
+
+
+// These are dummy variables so this code will compile.  This code is nowhere near finished
+uint8_t g_UART0_TX_empty_placeholder=1;
+
+void register_command(char * s, int process_id){
+	int i = 0;
+
+	registered_processes[number_of_registered_commands] = process_id;
+	for(i = 0; i < MAX_COMMAND_LENGTH; i++){
+		assert(i < MAX_COMMAND_LENGTH,"Invalid attempt to register a command that is too long.");
+
+		registered_commands[number_of_registered_commands][i]  = s[i];
+		//  We want to copy everything up to and including the null
+		if(s[i] == 0)
+			break;
+
+	}
+	number_of_registered_commands++;
+	assert(number_of_registered_commands < MAX_NUMBER_OF_REGISTERABLE_COMMANDS, "Too many commands registered");
+}
+
+int get_index_of_matching_command(){
+	/*  Returns the first index of a complete command that
+	 exists as a prefix of the current command buffer
+	 */
+	int i = 0;
+	int j = 0;
+ 	for(i = 0; i < number_of_registered_commands; i++){
+		for(j = 0; j < MAX_NUMBER_OF_REGISTERABLE_COMMANDS; j++){
+			if(registered_commands[i][j] == 0){
+				//  We are at the end of the command so it must have matched so far.
+				assert(j > 0,"We just matched an empty command.  This is probably not right.");
+				return i;
+			}else if(j >= current_command_length){
+				//  We reached the end of the command buffer, there is nothing to check
+				break;
+			}else if(!(registered_commands[i][j] == current_command_buffer[i])){
+				// This command did not match
+				break;
+			}	
+		}
+	}
+	//  No matching command found
+	return -1;
+} 
 
 void keyboard_command_decoder(void){
  	while(1){
+		int aaa = 0;
 		int sender_id = -1;
+		// Get our new message
 		void * message = k_receive_message(&sender_id);
+		//  Point to the data
+		char * pChar = get_message_data(message);
+
+		// If the buffer is full, they are not part of any valid command so we don't care
+		if(current_command_length <= MAX_COMMAND_LENGTH){
+			//  Put the character we received into the buffer
+			current_command_buffer[current_command_length] = *pChar;
+			current_command_length++;
+		}
+
+		// Did they type a newline?
+		if(*pChar == 10){
+			int matched_command = get_index_of_matching_command();
+	
+			//  Does the thing in the buffer match a command that was registered? 
+			if(matched_command > -1){
+			 	  //  The user type a command that we recognized
+				  aaa++;
+			}
+
+			// Reset the buffer for new commands
+			current_command_length = 0;
+		}
+
 		release_memory_block(message);		
 	}
 }

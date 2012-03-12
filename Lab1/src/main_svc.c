5a24e29c666e990f03c997c05bfcd2c2f7681af3
Robert Elder <robert@robertelder.ca>
2012-03-12 00:40:05 -0400

    Deleted comments that were from the sample code

--- Lab1/src/main_svc.c
+++ Lab1/src/main_svc.c
@@ -1,11 +1,3 @@
-/**
- * @brief:  main_svc.c, demonstrate svc as a gateway to os functions
- * @author: Yiqing Huang
- * @date: 2012/01/08
- * NOTE: standard C library is not allowed in the final kernel code
- *       it is OK if you only use the C library for debugging purpose 
- *       during the development/prototyping phase
- */
 
 #include <LPC17xx.h>
 #include <system_LPC17xx.h>
